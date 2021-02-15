54eb239f4e611d6ca6ad1910e23bf4f612dffccf
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-02-15 10:16:03 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentRepository';
+
+const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked!' });
+  }
+
+  const appointment = appointmentRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
