823b270e92c7470f948c44861983d0a23d6552fb
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-02-19 00:14:53 -0300

    Routes are now authenticated with JWT tokens

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '172d8d20e4262d296bfc03a867581e75',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
-import usersRouter from '../routes/users.routes';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,16 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination!');
     }
 
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
     return {
       user,
+      token,
     };
   }
 }
